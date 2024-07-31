9c79fed779dd37b9da15127d4eeb861e0d75be5f
Tre <38577648+Kitearly@users.noreply.github.com>
2024-07-31 21:21:19 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-WORKDIR=/root/blog
+WORKDIR=/data
 cd ${WORKDIR}
 
 # 如果当前目录为空，则新建一个博客网站
 if [ "`ls -A $WORKDIR`" = "" ]; then
     echo ">>>>>>>>>>> 当前目录为空,初始化一个博客网站"
     hexo init ./
-    echo ">>>>>>>>>>> 安装依赖和hexo插件"
+    echo ">>>>>>>>>>> 安装依赖"
     npm config set registry http://registry.npm.taobao.org/
     npm install
-    # 插件安装写在这里
-    npm install --save hexo-generator-searchdb \
-        hexo-word-counter \
-        hexo-generator-sitemap
-    echo ">>>>>>>>>>> 安装Next主题"
-    git clone https://github.com/next-theme/hexo-theme-next.git themes/next
-    sed -i 's/theme: landscape/theme: next/g' _config.yml
 else
     echo ">>>>>>>>>>> 使用已有的博客"
 fi
